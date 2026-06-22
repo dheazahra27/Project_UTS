7a02e2158a231951847f266bcdf2a7d01edd18de
c030324084-Erza <c030324084@mahasiswa.poliban.ac.id>
2026-06-22 17:36:30 +0800

    Pembuatan SplashScreen

--- app/src/main/java/com/example/project_uts/SplashScreen.kt
+++ app/src/main/java/com/example/project_uts/SplashScreen.kt
@@ -1,6 +1,9 @@
 package com.example.project_uts
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +14,25 @@ class SplashScreen : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_splash_screen)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Waktu delay untuk SplashScreen dalam milidetik (contoh: 3000 = 3 detik)
+        val delayTime = 3000L
+
+        // Menggunakan Handler untuk menunda eksekusi kode
+        Handler(Looper.getMainLooper()).postDelayed({
+            // Intent untuk berpindah dari SplashScreen ke LoginActivity
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+
+            // Mengakhiri activity ini agar pengguna tidak bisa kembali ke splash screen
+            // saat menekan tombol back (kembali)
+            finish()
+        }, delayTime)
     }
 }
